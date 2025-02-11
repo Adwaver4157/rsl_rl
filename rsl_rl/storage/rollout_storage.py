a85da1319c9a0b64eb47bf07516c16404e1562f8
Adwaver4157 <adwaver4157@gmail.com>
2025-02-12 05:43:51 +0900

    chore: fix key

--- rsl_rl/storage/rollout_storage.py
+++ rsl_rl/storage/rollout_storage.py
@@ -61,8 +61,8 @@ class RolloutStorage:
         # Core
         # self.observations = torch.zeros(num_transitions_per_env, num_envs, *obs_shape, device=self.device)
         self.observations = {
-            "vector": torch.zeros(num_transitions_per_env, num_envs, obs_shape[0], device=self.device),
-            "image": torch.zeros(num_transitions_per_env, num_envs, *obs_shape[1], device=self.device),
+            "obs": torch.zeros(num_transitions_per_env, num_envs, obs_shape[0], device=self.device),
+            "img_obs": torch.zeros(num_transitions_per_env, num_envs, *obs_shape[1], device=self.device),
         }
         if privileged_obs_shape[0] is not None:
             self.privileged_observations = torch.zeros(num_transitions_per_env, num_envs, *privileged_obs_shape, device=self.device)
