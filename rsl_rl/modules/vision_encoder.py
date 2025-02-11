6234b9db23a57ebc85f0b511efa59840ed836dde
Adwaver4157 <adwaver4157@gmail.com>
2025-02-12 04:05:04 +0900

    chore: fix typo

--- rsl_rl/modules/vision_encoder.py
+++ rsl_rl/modules/vision_encoder.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
         #                      std=[0.229, 0.224, 0.225])  # 必要に応じて正規化
     ])
     # 画像に変換を適用
-    dummy_input = np.randn(3, 640, 480)
+    dummy_input = np.random.randn(3, 640, 480)
     img_tensor = transform(dummy_input)  # 形状: (3, 128, 128)
 
     # バッチ次元を追加：モデルの入力は通常 (B, C, H, W) となるので
