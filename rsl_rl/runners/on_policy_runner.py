69a843ce2d380cde4f2c1206e67934b944525e65
Adwaver4157 <adwaver4157@gmail.com>
2025-02-25 03:00:56 +0900

    chore: change save video path

--- rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/runners/on_policy_runner.py
@@ -163,12 +163,12 @@ class OnPolicyRunner:
         self.current_learning_iteration += num_learning_iterations
         self.save(os.path.join(self.log_dir, 'model_{}.pt'.format(self.current_learning_iteration)))
         if self.vis:
-            self.env.fixed_camera.stop_recording(save_to_filename='logs/videos/video_train.mp4', fps=60)
-            self.env.follower_camera.stop_recording(save_to_filename='logs/videos/follow_video_train.mp4', fps=60)
+            self.env.fixed_camera.stop_recording(save_to_filename=os.path.join(self.log_dir, 'video_train.mp4'), fps=60)
+            self.env.follower_camera.stop_recording(save_to_filename=os.path.join(self.log_dir, 'follow_video_train.mp4'), fps=60)
             # env.head_camera.stop_recording(save_to_filename='logs/videos/head_video_train.mp4', fps=60)
             if hasattr(self.env, 'head_cameras'):
                 for i in range(len(self.env.head_cameras)):
-                    self.env.head_cameras[i].stop_recording(save_to_filename=f'logs/videos/head_video_train_{i}.mp4', fps=60)
+                    self.env.head_cameras[i].stop_recording(save_to_filename=os.path.join(self.log_dir, f'head_video_train_{i}.mp4'), fps=60)
 
     def log(self, locs, width=80, pad=35):
         self.tot_timesteps += self.num_steps_per_env * self.env.num_envs
